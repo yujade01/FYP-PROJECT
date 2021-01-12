ab3bd8d4a838663869c0ec570d1c674efcaa7bd9
yujade01 <yujade77@gmail.com>
2021-01-12 16:08:37 +0800

    set maxlength for login form

--- login.php
+++ login.php
@@ -14,18 +14,19 @@
         <form action="loginsystem.php" method="post">
             <div class="form-group">
                 <label>Username</label>
-                <input type="text" name="userID" class="form-control" value="">
+                <input type="text" name="userID" class="form-control" maxlength="10">
           
             </div>    
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control">
+                <input type="password" name="password" class="form-control" maxlength="25">
                 
             </div>
             <div class="form-group">
-                <button type="submit" name="login" class="btn btn-primary" value="Login">
+                <button type="submit" name="login" class="btn btn-primary button" value="Login">
             </div>
             <p>Don't have an account? <a href="signup.php">Sign up now</a>.</p>
+            <p>Forgot Password? <a href="forgotPWForm.php">Reset Password now</a>.</p>
         </form>
     </div>    
 </body>
