b7717de8c585af8e34349b6ea27cfe5ff4da69ef
BingWei <bingwei1975@gmail.com>
2021-01-23 21:08:47 +0800

    navigation bar

---
+++ navigation.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nav Bar With Icons</title>
+    <link rel = "stylesheet" type="text/css" href = "navigation.css">
+    <link href = "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity ="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin = "anonymous">
+</head>
+
+<body>
+    <ul>
+
+    <li>
+    <a href="#"><img src="pchublogo.png" height="90px" width="90px"></a>
+    </li>
+
+    <li>
+    <a href="#">
+    <div class = "icon">
+        <i class="fa fa-home" aria-hidden="true"></i>
+        <i class="fa fa-home" aria-hidden="true"></i>
+    </div>
+    <div class="name"><span data-text = "Home">Home</span></div>
+    </a>
+    </li>
+
+    <li>
+    <a href="#">
+    <div class = "icon">
+        <i class="fa fa-laptop" aria-hidden="true"></i>
+        <i class="fa fa-laptop" aria-hidden="true"></i>
+    </div>
+    <div class="name"><span data-text = "Laptop">Laptop</span></div>
+    </a>
+    </li>
+
+    <li>
+    <a href="#">
+    <div class = "icon">
+        <i class="fa fa-desktop" aria-hidden="true"></i>
+        <i class="fa fa-desktop" aria-hidden="true"></i>
+    </div>
+    <div class="name"><span data-text = "Monitor">Monitor</span></div>
+    </a>
+    </li>
+
+    <li>
+    <a href="#">
+    <div class = "icon">
+        <i class="fa fa-mouse-pointer" aria-hidden="true"></i>
+        <i class="fa fa-mouse-pointer" aria-hidden="true"></i>
+    </div>
+    <div class="name"><span data-text = "Mouse">Mouse</span></div>
+    </a>
+    </li>
+
+    <li>
+    <a href="#">
+    <div class = "icon">
+        <i class="fa fa-keyboard-o" aria-hidden="true"></i>
+        <i class="fa fa-keyboard-o" aria-hidden="true"></i>
+    </div>
+    <div class="name"><span data-text = "Keyboard">Keyboard</span></div>
+    </a>
+    </li>
+
+    <li>
+    <a href="#">
+    <div class = "icon">
+    <i class="fa fa-print" aria-hidden="true"></i>
+    <i class="fa fa-print" aria-hidden="true"></i>
+    </div>
+    <div class="name"><span data-text = "Printer">Printer</span></div>
+    </a>
+    </li>
+
+    <li>
+    <a href="#">
+    <div class = "icon">
+    <i class="fa fa-hdd-o" aria-hidden="true"></i>
+    <i class="fa fa-hdd-o" aria-hidden="true"></i>
+    </div>
+    <div class="name"><span data-text = "Accessories">Accessories</span></div>
+    </a>
+    </li>
+
+
+
+
+    </ul>
+
+</body>
+</html>
