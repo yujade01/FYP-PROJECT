f0874ccaa9d58ada83fc9d9272d01583104ca882
yujade01 <yujade77@gmail.com>
2021-01-29 18:47:16 +0800

    add checkbox for sorting

---
+++ sort.php
@@ -0,0 +1,21 @@
+<html>
+<fieldset>
+    <dl">
+        <dt><label for="sort">Sorting:</label></dt>
+        <dd>
+            <input type="checkbox" name="sort1" value="atoz"/>
+            <label for="alphabetA-z" class="">Alphabet A-Z</label>
+            <input type="checkbox" name="sort2" value="ztoa"/>
+            <label for="alphabetA-z" class="">Alphabet Z-A</label>
+            <p>Price: </p>
+            <input type="checkbox" name="sort3" value="lowtohigh"/>
+            <label for="priceLowtoHigh" class="">Low to High</label>
+            <input type="checkbox" name="sort4" value="hightolow"/>
+            <label for="priceLowtoHigh" class="">High to Low</label>
+
+            <input type="submit" name="sort" class="btn btn-info" value="SORT">
+        </dd>
+    </dl>
+</fieldset>
+
+</html>
