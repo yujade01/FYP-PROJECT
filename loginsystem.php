eed662467d956672c19cdc4de4c3bc7e16de5672
yujade01 <yujade77@gmail.com>
2021-02-02 01:40:38 +0800

    add session variables loggedin and username & alert box

--- loginsystem.php
+++ loginsystem.php
@@ -19,11 +19,20 @@
     $result = mysqli_query($conn, $sql);  
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
     $count = mysqli_num_rows($result);  
-      
-    if($count == 1){  
-        echo "<h1><center> Login successful </center></h1>";
-    }  
-    else{  
-        echo "<h1> Login failed. Invalid username or password.</h1>";  
+    
+    //check query
+    if ($result == true)  {
+        session_start();
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        ?>
+        <script> alert("Login successfully"); window.location.href = "welcome.php";</script>
+
+        <?php
+        //header("Location: welcome.php");
+      }else{
+        ?>
+        <script> alert("Failed to login.")</script>
+        <?php
     }     
 ?>
