087691e69112fba0354129b5b2bce7ff8f3f2ad9
yujade01 <yujade77@gmail.com>
2021-02-02 16:39:25 +0800

    add class and css for button

--- navigation.php
+++ navigation.php
@@ -5,6 +5,58 @@
     <link rel = "stylesheet" type="text/css" href = "navigation.css">
     <script src="https://kit.fontawesome.com/22e170816e.js" crossorigin="anonymous"></script>
     <link href = "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity ="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin = "anonymous">
+    <style>
+        .but {
+        display: inline-block;
+        padding: 6px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+        font-weight: normal;
+        line-height: 1.42857143;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        -ms-touch-action: manipulation;
+            touch-action: manipulation;
+        cursor: pointer;
+        background-image: none;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        }
+        .btn-primary {
+        color: #fff;
+        background-color: #337ab7;
+        border-color: #2e6da4;
+        }
+        .btn-primary:focus,
+        .btn-primary.focus {
+        color: #fff;
+        background-color: #286090;
+        border-color: #122b40;
+        }
+        .btn-primary:hover {
+        color: #fff;
+        background-color: #286090;
+        border-color: #204d74;
+        }
+
+        .btn-danger {
+        color: #fff;
+        background-color: #d9534f;
+        border-color: #d43f3a;
+        }
+        .btn-danger:focus,
+        .btn-danger.focus {
+        color: #fff;
+        background-color: #c9302c;
+        border-color: #761c19;
+        }
+        .btn-danger:hover {
+        color: #fff;
+        background-color: #c9302c;
+        border-color: #ac2925;
+        }
+    </style>
 </head>
 
 <body>
@@ -92,13 +144,13 @@
         {
             ?>
             <li>
-                <a href="login.php"><input type="submit" name="loginBtn" value="LOGIN"></input></a>
+                <a href="login.php"><input type="submit" class="but btn-primary" name="loginBtn" value="LOGIN"></input></a>
             </li>
             <?php
         }else{
             ?>
             <li style="float:right">
-                <a href="logout.php"><input type="submit" name="logoutBtn" value="LOGOUT"></a>
+                <a href="logout.php"><input type="submit" class="but btn-danger" name="logoutBtn" value="LOGOUT"></a>
             </li>
             <?php
         }
