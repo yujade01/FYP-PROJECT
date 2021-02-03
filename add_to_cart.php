2be5d0c8ec39c9d629a8e8656219d199c8c11771
yujade01 <yujade77@gmail.com>
2021-02-03 20:41:59 +0800

    add search id query

--- add_to_cart.php
+++ add_to_cart.php
@@ -10,14 +10,31 @@ session_start();
         $imgDir = $_POST["imgDir"];
 
         $conn = mysqli_connect("localhost", "root", "", "pchub");
+        //update product quantity
+        
+        //search userID and productID in table cart
+        $search_id = "SELECT * FROM cart where userID = '$userid' and productID = '$prod_id'";
+        $search_result = mysqli_query($conn, $search_id);
         //insert into cart table
         $sql = "INSERT INTO cart value('', '$userid', '$prod_id', '$quantity', '$price', '$imgDir')";
         $result = mysqli_query($conn, $sql);
 
-        if($result == true)
+        if($search_result == true)
+        {  
+            //update qty in existing record
+            $update_qty = "UPDATE cart set qty += '$quantity' where userID = '$userid' and productID = '$prod_id'";
+            $update_result = mysqli_query($conn, $update_qty);
+
+            if($update_result == true)
+            {
+                ?>
+            <script> alert("Product added to cart."); window.location.href = "cart.php";</script>
+            <?php
+            }
+        }
+        else if($result == true)
         {
             ?>
-
             <script> alert("Product added to cart."); window.location.href = "cart.php";</script>
             <?php
         }else{
