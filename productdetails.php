77317761f3e3be4e4c764d60a0b56bc8c5b878c1
BingWei <bingwei1975@gmail.com>
2021-02-05 15:16:42 +0800

    product details css, add to cart button, quantity

--- productdetails.php
+++ productdetails.php
@@ -14,67 +14,65 @@
      
     //    if ($productCount > 0) {
  	 	// get all the product details
-    $row = mysqli_fetch_assoc($sql);
- 	$product_name = $row["productName"];
- 	$price = $row["productPrice"];
-    $details = $row["productDesc"]; 
-    $category = $row['categoryID'];
-    $imagePath = $row['imgDir'];
-        
-        
-		 
-//  	} else {
-//  		echo "That item does not exist.";
-//  	    exit();
-//  	}
-		
-//  } else {
-//  	echo "Data to render this page is missing.";
-//  	exit();
-//  }
-//   mysql_close();
-
+        $row = mysqli_fetch_assoc($sql);
+ 	      $product_name = $row["productName"];
+ 	      $price = $row["productPrice"];
+        $details = $row["productDesc"]; 
+        $category = $row['categoryID'];
+        $imagePath = $row['imgDir'];
     }
+
  ?> 
 
 <!DOCTYPE html>
 <html>
 <head>
 
-<title><?php echo $product_name; ?></title>
+  <title><?php echo $product_name; ?></title>
+
+  <link rel = "stylesheet" type="text/css" href="productdetails.css">
+
 
 </head>
+
 <body>
 
+  <!-- Navigation bar -->
+  <?php include ('navigation.php');?>
 
-<div align="center" id="mainWrapper">
+  <a href="showproduct.php" ><input type="button" class="back-button" value="< BACK"></input></a>
 
-  <!--<?php include_once("template_header.php");?>-->
+  <main class="container">
+    <div class="left-column">
+        <img src="<?php echo $imagePath ?>" width="500" height="500" alt="<?php echo $product_name; ?>" />
+    </div>
 
-  <div id="pageContent">
+    <div class = "right-column">
+    <div class = "product-desc">
+        <h1><?php echo $product_name; ?></h1>
+        <p class ="desc"><?php echo $details; ?></p>
+    </div>
 
-  <table width="100%" border="0" cellspacing="0" cellpadding="15">
-
-  <tr>
-    <td width="19%" valign="top"><img src="<?php echo $imagePath ?>" width="142" height="188" alt="<?php echo $product_name; ?>" /><br />
-    <td width="81%" valign="top"><h3><?php echo $product_name; ?></h3>
-      <p><?php echo "$".$price; ?><br/>
-        <br />
-        <?php echo $category; ?> <br />
-<br />
-         <?php echo $details; ?> 
-<br />
-        </p>
-      <!--
-      <form id="form1" name="form1" method="post" action="cart.php">
-        <input type="hidden" name="pid" id="pid" value="<?php echo $id; ?>" />
-        <input type="submit" name="button" id="button" value="Add to Shopping Cart" />
+    <div>
+      <form action="">
+        <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" value="1" min="1">
       </form>
-      </td>
-    </tr>
-</table>
+    </div>
+
+    <div class="product-price">
+      <p class = "price"><?php echo "RM ".$price; ?><br/><p>
+    </div>
+
+      <form  method="post" action="">
+        <input type="hidden" name="pid" id="pid" value="<?php echo $id; ?>" />
+        <input type="submit" name="button" id="button" value="Add to Shopping Cart" /> 
+
+      </form>
+ 
   </div>
-</div> -->
+  </main>
+
 
 </body>
 </html>
