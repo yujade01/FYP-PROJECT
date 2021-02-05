ee09a8f6c3b376c979d9db9c2c225d7b6e903547
yujade01 <yujade77@gmail.com>
2021-02-05 15:16:35 +0800

    update qty from table product and error reporting

--- add_to_cart.php
+++ add_to_cart.php
@@ -12,31 +12,49 @@ session_start();
         $conn = mysqli_connect("localhost", "root", "", "pchub");
         //update product quantity
         
-        //search userID and productID in table cart
+        //search whether similar product exists in table cart based on userID
         $search_id = "SELECT * FROM cart where userID = '$userid' and productID = '$prod_id'";
         $search_result = mysqli_query($conn, $search_id);
         //insert into cart table
         $sql = "INSERT INTO cart value('', '$userid', '$prod_id', '$quantity', '$price', '$imgDir')";
         $result = mysqli_query($conn, $sql);
+        //update stock from table product
+        $update_stock = "UPDATE product set qty-=$quantity where userID = '$userid' and productID = '$prod_id'";
 
+        //if query found the row
         if($search_result == true)
         {  
             //update qty in existing record
             $update_qty = "UPDATE cart set qty += '$quantity' where userID = '$userid' and productID = '$prod_id'";
             $update_result = mysqli_query($conn, $update_qty);
 
+            //if update qty successfully
             if($update_result == true)
             {
+                $update_stock_result = mysqli_query($conn, $update_stock);
+
+                if($update_stock_result == true)
+                {
                 ?>
-            <script> alert("Product added to cart."); window.location.href = "cart.php";</script>
+                <script> alert("Product added to cart."); window.location.href = "cart.php";</script>
             <?php
+                }else{
+                    error_reporting(E_ALL);
+                }
+            }else{
+                error_reporting(E_ALL);
             }
-        }
-        else if($result == true)
+        }else if($search_result == false && $result == true)
         {
+            $update_stock_result = mysqli_query($conn, $update_stock);
+            if($update_stock_result == true)
+            {
             ?>
             <script> alert("Product added to cart."); window.location.href = "cart.php";</script>
             <?php
+            }else{
+                error_reporting(E_ALL);
+            }
         }else{
             ?>
             <script> alert("Failed to add product to cart"); window.location.href = "showproduct.php";</script>
