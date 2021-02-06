026cb161572c1d05695be6db482b8871cd635248
yujade01 <yujade77@gmail.com>
2021-02-06 19:41:28 +0800

    update qty & subtotal php file

---
+++ update_qty_price.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    $username = $_SESSION["username"];
+    $productID = $_POST["pid"];
+    $qty = $_POST['qty'];
+
+    if(isset($_POST['decrement']))
+    {
+        $updated_qty = $qty - 1;
+
+    }else if(isset($_POST['increment']))
+    {
+        $updated_qty = $qty + 1;   
+    }
+    
+    if($updated_qty <=0)
+    {
+        //limit quantity to 0
+        $updated_qty = 0;
+        ?>
+        <script> window.location.href="cart.php"; </script>
+        <?php
+    }else{
+        $conn = mysqli_connect("localhost", "root", "", "pchub");
+        $sql = "UPDATE cart set quantity = '$updated_qty' where userID = '$username' and productID = '$productID'";
+
+        $result = mysqli_query($conn, $sql);
+        
+        if($result == true){
+            header("Location: cart.php");
+        }else{
+            ?>
+            <script> alert("Something went wrong."); </script>
+            <?php
+            header("Location: cart.php");
+        }
+    }
+?>
