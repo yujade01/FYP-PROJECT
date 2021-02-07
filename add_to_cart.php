e894c81dc4be1bbc5f6e21924f103d60dafc2a16
yujade01 <yujade77@gmail.com>
2021-02-08 03:41:09 +0800

    removed unused query and change to Username

--- add_to_cart.php
+++ add_to_cart.php
@@ -3,7 +3,7 @@ session_start();
 
     if(isset($_POST["add_to_cart"]))
     {
-        $userid = $_SESSION["username"];
+        $username = $_SESSION["username"];
         $prod_id = $_POST["pid"];
         $quantity = $_POST["quantity"];
         $price = $_POST["price"];
@@ -13,15 +13,12 @@ session_start();
         //update product quantity
         
         //search whether similar product exists in table cart based on userID
-        $search_id = "SELECT * FROM cart WHERE userID = '$userid' AND productID = '$prod_id'";
+        $search_id = "SELECT * FROM cart WHERE Username = '$username' AND productID = '$prod_id'";
         $search_result = mysqli_query($conn, $search_id);
         $count = mysqli_num_rows($search_result);
 
         //insert into cart table
-        $sql = "INSERT INTO cart VALUES('', '$userid', '$prod_id', '$quantity', '$price', '', '$imgDir')";
-        //update stock from table product
-        $update_stock = "UPDATE product set quantity = (quantity - '$quantity') 
-        WHERE productID = '$prod_id'";
+        $sql = "INSERT INTO cart VALUES('', '$username', '$prod_id', '$quantity', '$price', '', '$imgDir')";
 
         //if query found the row
         if($count > 0)
@@ -33,19 +30,9 @@ session_start();
             //if update qty successfully
             if($update_result == true)
             {
-                //update quantity
-                $update_stock_result = mysqli_query($conn, $update_stock);
-
-                if($update_stock_result == true)
-                {
                 ?>
                 <script> alert("Product added to cart."); window.location.href = "cart.php";</script>
             <?php
-                }else{
-                    ?>
-                    <script> alert("Failed to update stock."); window.location.href = "cart.php";</script>
-                    <?php
-                }
             }else{
                 ?>
                     <script> alert("Failed to update quantity."); window.location.href = "cart.php";</script>
@@ -53,19 +40,11 @@ session_start();
             }
         }else if($count == 0)
         {
-            $update_stock_result = mysqli_query($conn, $update_stock);
-            if($update_stock_result == true)
-            {
                 //insert item into cart
                 $result = mysqli_query($conn, $sql);
             ?>
             <script> alert("Product added to cart."); window.location.href = "cart.php";</script>
             <?php
-            }else{
-                ?>
-                    <script> alert("Something went wrong."); window.location.href = "cart.php";</script>
-                    <?php
-            }
         }else{
             ?>
             <script> alert("Failed to add product to cart"); window.location.href = "showproduct.php";</script>
