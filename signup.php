bcdd1ae2d56e9ceb73af6133e0ad7098545dfd92
yujade01 <yujade77@gmail.com>
2021-02-07 23:38:13 +0800

    add new textbox your signup form

--- signup.php
+++ signup.php
@@ -19,28 +19,25 @@
     <div class="center">
         <h1>Hello, Welcome to Signup Page</h1>
 
-        <form action="signupsystem.php" method="post">
-        <br/><br/>
-        <label for="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="Eg: John" required>
-            <br><br>
-        <label for="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Eg: FyP3@1" required>
-            <br><br>
-        <label for="email">Email Address:</label>
-            <input type="text" id="email" name="email" placeholder="Eg: john123@gmail.com" required>
-            <br><br>
-        <label for="phone">Phone Number:</label>
-            <input type="text" id="phone" name="phone" placeholder="Eg: 0123456789" required/>
+        <form action="signupsystem.php" method="POST">
             <br/><br/>
-        <label for="address">House address:</label>
-            <input type="text" id="address" name="address" placeholder="Eg: 100, taman fyp, 32000 Selangor" required/>
-        <p>
-            <br/><br/>
-            <a href="login.php"><input type="button" class="btn btn-primary" name="back" value="BACK"></a>
-            <input type="submit" class="btn btn-success" name="signup" value="Signup"/>
-            <br/><br/>
-        </p>  
+            <label for="username">Username:</label>
+                <input type="text" id="username" name="username" placeholder="Eg: John" required>
+                <br><br>
+            <label for="password">Password:</label>
+                <input type="password" id="password" name="password" placeholder="Eg: FyP3@1" required>
+                <br><br>
+            <label for="email">Email Address:</label>
+                <input type="text" id="email" name="email" placeholder="Eg: john123@gmail.com" required>
+                <br><br>
+            <label for="phone">Phone Number:</label>
+                <input type="text" id="phone" name="phone" placeholder="Eg: 0123456789" required/>
+                <br/><br/>
+            <label for="address">House address:</label>
+                <input type="text" id="address" name="address" placeholder="Eg: 100, taman fyp, 32000 Selangor" required/>
+                <br/><br/>
+                <a href="login.php"><input type="button" class="btn btn-primary" name="back" value="BACK"></a>
+                <input type="submit" class="btn btn-success" name="signup" value="Signup"/>
         </form>
     </div>
         
