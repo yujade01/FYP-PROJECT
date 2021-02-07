5e7506e8a3d37630985caa0a1f6ea7023b3ab882
yujade01 <yujade77@gmail.com>
2021-02-08 03:37:27 +0800

    add alert box and change to Username

--- resetPwSystem.php
+++ resetPwSystem.php
@@ -5,26 +5,28 @@
         echo 'Not Connected To Server';
     }
 
-    $userID = $_POST['userID'];
+    $username = $_POST['username'];
     $PWtoReset = $_POST['newPw'];
     $PWConfirm = $_POST['confirmPw'];
 
-    $check = "SELECT * FROM account where userID = '$userID'";
+    $check = "SELECT * FROM account where Username = '$username'";
     $checkResult = mysqli_query($conn, $check);
 
     if(mysqli_num_rows($checkResult) >0)
     {
-        $sql = "UPDATE account SET userPassword = '$PWtoReset' where userID ='$userID'";
+        $sql = "UPDATE account SET userPassword = '$PWtoReset' where Username ='$username'";
         $result = mysqli_query($conn, $sql);
 
         if($result == true)
         {
-            echo "<h1>Password updated successfully.</h1>";
-            header('login.php');
+            ?>
+        <script> alert("Password updated successfully"); window.location.href = "login.php";</script>
+        <?php
         }else
         {
-            echo "<h1>Error updating password.</h1>";
-            header('resetPwSystem.php');
+            ?>
+            <script> alert("Failed to update password"); window.location.href = "login.php";</script>
+            <?php
         }
     }else{
         echo "<h1>User ID does not exists.</h1>";
